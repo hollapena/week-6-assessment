70996ed93df885848464f4a111a2cd168553ce39
Callen Schultz <callen.schultz@gmail.com>
2021-10-22 16:13:57 -0500

    Adding Corrections

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -1,4 +1,4 @@
-import { Builder, Capabilities, By} from "selenium-webdriver"
+import { Builder, Capabilities, By } from "selenium-webdriver"
 import { elementIsDisabled } from "selenium-webdriver/lib/until"
 import { isAssertionExpression } from "typescript"
 
@@ -24,6 +24,8 @@ test('I can start a game', async () => {
 test('I can add an X to the upper left square', async () => {
     let button = await (await driver).findElement(By.id('cell-0'));
     await button.click();
+
+    expect(await button.getText()).toContain('X')
     await driver.sleep(2000)
     
 });
@@ -31,6 +33,8 @@ test('I can add an X to the upper left square', async () => {
 test('I can add an X to the upper right square', async () => {
     let button = await (await driver).findElement(By.id('cell-2'));
     await button.click();
+
+    expect(await button.getText()).toContain('X')
     await driver.sleep(2000)
     
 });
@@ -38,16 +42,25 @@ test('I can add an X to the upper right square', async () => {
 test('I can add an X to the lower right square', async () => {
     let button = await (await driver).findElement(By.id('cell-8'));
     await button.click();
+    expect(await button.getText()).toContain('X')
     await driver.sleep(2000)
     
 });
 
-// test('I can see an O in square 1', async () => {
-//     let button = driver.findElement(By.css('td')[2]);
-//     Assert.strictEqual(await button.getText(), 'O');
-// });
+ test('I can see an O in square 1', async () => {
+     let button = driver.findElement(By.id('cell-1'));
+     expect(await button.getText()).toContain('O')
+        await driver.sleep(2000)
+ });
 
-// // test('I can see an O in square 3', async () => {
-// //     let button = driver.findElement(By.css('td')[4]);
-// //     assert.strictEqual(await button.getText(), 'O');
-// // });
+test('I can see an O in square 3', async () => {
+    let button = driver.findElement(By.id('cell-3'));
+    expect(await button.getText()).toContain('O')
+        await driver.sleep(2000)
+});
+
+test('I can see an O in square 4', async () => {
+    let button = driver.findElement(By.id('cell-4'));
+    expect(await button.getText()).toContain('O')
+        await driver.sleep(2000)
+});
